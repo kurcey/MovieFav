5af8a0ff34eae25dde88fb8e5bc4df24595928c4
kurt <kurcey@yahoo.com>
2019-06-29 06:20:38 -0400

    Initial Commit

---
+++ movieFavs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieFavs
+//
+//  Created by user152630 on 6/29/19.
+//  Copyright © 2019 user152630. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
